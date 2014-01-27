21102d9d9f81b85e2a36153a8b7e7dea5d67403d
Krasimir Georgiev <void.unsigned@gmail.com>
2014-01-28 00:27:41 +0200

    Added bootstrap and navbar. CSS-ed the login page.

--- mvc/application/views/layout/layout.php
+++ mvc/application/views/layout/layout.php
@@ -2,47 +2,74 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	
-	<?php if (isset($params['css']) && is_array($params['css'])) :?>
+
+        <!-- START Twitter Bootstrap -->
+
+            <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+
+            <!-- Optional theme -->
+            <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+
+            <!-- Latest compiled and minified JavaScript -->
+            <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+            <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+
+        <!-- END Twitter Bootstrap -->
+        
+        <?php if (isset($params['css']) && is_array($params['css'])) :?>
 	<?php foreach($params['css'] as $css) : ?>
 		<link href="<?=$css ?>" rel="stylesheet">	
 	<?php endforeach; ?>
 	<?php else :?>
 	<link href="assets/main.css" rel="stylesheet">
 	<?php endif; ?>
-	<title><?php echo (($this->pageTitle != null)?$this->pageTitle . ' | ':'')?> Vacation Request </title>
+	<title><?php echo (($this->pageTitle != null)?$this->pageTitle . ' | ':'')?> Vacations </title>
 </head>
 <body>
-        
-    <?php if (isset($_SESSION['user_id']) && strlen($_SESSION['user_id'])):?>
-    	<div class="toolbar">
-            <div id="toolbar">
-                <h1>
-                    Hello, <?=$_SESSION['name']?>.
-                    <a href="index.php?q=users/logout">Logout</a>
-                </h1>
-                <!--
-                <?php echo '<h1><a href="index.php?q=users/logout">Hello, ' . $_SESSION['name']. '!  Logout ' . '</a></h1>';?>
-                -->
-                <div class="clearfix"></div>
+<div id="wrap">
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">vacation</a>
             </div>
-   		</div>
-	<?php endif;?>
-        
+
+<?php if (isset($_SESSION['user_id']) && strlen($_SESSION['user_id'])):?>
+
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Link 1</a></li>
+                    <li><a href="#">Link 2</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                <li class="navbar-text">Hello, <?=$_SESSION['name']?>.</li>
+                    <li><a href="index.php?q=users/logout">Logout</a></li>
+                </ul>
+            </div>
+
+<?php endif;?>
+        </nav>
+
+
 	<div id="wrapper">
 		
-		<header>
-			<h1 class="logo"><a href="index.php"> Vacation Request </a></h1>
-		</header>
+            <?php echo $content; ?>
 		
-		<?php echo $content; ?>
-		
-		<footer>
-			<div id="left_footer">WWW Технологии 2014</div>
-			<div id="right_footer">ФМИ 2014</div>
-		</footer>
+        </div>
 
-	</div>
+    </div>
+
+</div> <!-- wrap -->
+<!-- Make footer stay down! -->
+<footer>
+    <div class="container">
+        <div class="footer-in">
+            <div class="pull-left">WWW Технологии 2014</div>
+            <div class="pull-right">ФМИ 2014</div>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
 <!-- Hosting24 Analytics Code -->
