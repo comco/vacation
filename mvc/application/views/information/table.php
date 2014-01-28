884866e6c52c9b4233a5d0930a4df827875aa1fa
Martina Velichkova <martina.bilcheva@gmail.com>
2014-01-28 15:32:57 +0200

    CSS for information

--- mvc/application/views/information/table.php
+++ mvc/application/views/information/table.php
@@ -1,16 +1,35 @@
-<div id="history_table">
-	<table class="table" border=2>
-		<tbody>
+<head>
+	<style>
+			.history_table {padding-top: 50px}
+			th {font-family: "Helvetica Neue",Helvetica,Arial,sans-serif; font-size: 12px;}
+			thead { border-color: rgb(255, 255, 255)}
+       		thead th:first-child {border-top-left-radius: 4px; border-color: rgb(255, 255, 255)}
+			thead th:last-child {border-top-right-radius: 4px;  border-color: rgb(255, 255, 255)}
+          	thead th {background: linear-gradient(to bottom, #80B2E6 0%, #4D94DB 0%, #0066CC 100%) repeat scroll 0% 0% transparent;
+           				font-family: 'Open Sans',serif;
+						color: rgb(255, 255, 255);
+						font-size: 12px;
+						font-weight: bold;}
+
+	 </style>
+    
+</head>
+
+<div class="history_table">
+	<legend>Vacation History</legend>
+	<table class="table table-bordered table-hover">
+		
 			<?php
 
-			echo "<tr> <th>FROM</th> <th>TO</th> <th>DURATION</th> <th>TYPE</th> <th>STATUS</th> </tr>";
+			echo "<thead> <tr> <th>FROM</th> <th>TO</th> <th>DURATION</th> <th>TYPE</th> <th>STATUS</th> </tr> </thead>";
+			echo "<tbody>";
 				foreach ($params as $row ) {
 					$class = "";
 					if ($row['status'] == 'accepted') {
 						$class = 'class="success"';
 					}
 					else if ($row['status'] == 'pending') {
-						$class = 'class="active"';
+						$class = 'class="warming"';
 					}
 					else if ($row['status'] == 'rejected') {
 						$class = 'class="danger"';
