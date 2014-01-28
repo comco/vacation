9e9b78a4acea291937815afddcae35ba36a25aec
Pavlina Gatova <p.gatova@gmail.com>
2014-01-28 15:12:42 +0200

    add view and delete buttons

--- mvc/application/views/users/manageUsers.php
+++ mvc/application/views/users/manageUsers.php
@@ -6,6 +6,12 @@
 	<link href="assets/main.css" rel="stylesheet">
         <?php endif; ?>
 
+<style>
+            .table {width:600px;margin-left:280px;}
+            #delete_button{width:70px;margin:5px;color:red;}
+            #view_button{width:70px;margin:5px;}
+</style>
+
 <div class="container">
     <section class="login-form">
         <form method= "post" action="index.php?q=users/addNewUser"> 
@@ -16,21 +22,43 @@
     </section>
 </div>
 
-<div id="history_table">
-	<table class="table" border=2>
-		<tbody>
-			<?php
+<div>
+	<form>
+		<table class="table" border=2>
+			<tbody>
+				<?php
 
-			echo "<tr> <th>USERNAME</th> <th>NAME</th> <th>EMAIL</th> <th>OPTIONS</th> </tr>";
-				foreach ($params as $row) {
-					echo "<tr " . $class . "> "
+				echo "<tr> <th>USERNAME</th> <th>NAME</th> <th>EMAIL</th> <th>OPTIONS</th> </tr>";
+					foreach ($params as $row) {
+						echo "<tr " . $class . "> "
 							. "<th>" . $row['username']. "</th>"
 							. "<th>" . $row['name']. "</th>"
 							. "<th>" . $row['email']. "</th>"
-							. "<th>" . $row['']. "</th>"
-						. "</tr>";
+							. "<th>";
+				?>
+						<input id="view_button" class="btn btn-inline btn-primary" type="button" value="View" onclick='alert("Hello World!")'>
+						<input id="delete_button" class="btn btn-inline btn-primary" type="button" value="Delete">
+				<?php
+					 	echo "</th></tr>";
 				}
 			?>
-		</tbody>
-	</table>
+			</tbody>
+		</table>
+	</form>
 </div>
+
+
+<script type="text/javascript">
+
+// var button;
+// button = document.getElementById('view_button');
+// button.onclick = function () { 
+// 	
+// };
+
+var deleteButton;
+deleteButton = document.getElementById('delete_button');
+deleteButton.onclick = function () { 
+	
+};
+</script>
