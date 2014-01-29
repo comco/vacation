52fd86a5876380d8418a57b0b23f3447a117ffb1
Pavlina Gatova <p.gatova@gmail.com>
2014-01-29 12:05:18 +0200

    remove var_dump()

--- mvc/application/models/Days.php
+++ mvc/application/models/Days.php
@@ -5,7 +5,7 @@ include_once 'config/db.php';
 class Days {
     public static function addDays($user_id, $type, $days) 
     {
-        var_dump($user_id, $type, $days);
+//         var_dump($user_id, $type, $days);
         $conn = new PDO('mysql:host=' . DB_SERVER . ';dbname='.DB_NAME, DB_USERNAME, DB_PASSWORD);
         $conn->beginTransaction();
 
